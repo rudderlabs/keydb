a9793fa9d3fa48c2e8cb1a21c1ae7d445f7f6102
Francesco Casula <fra.casula@gmail.com>
2025-06-13 10:42:28 +0200

    chore: go imports

--- node/node_test.go
+++ node/node_test.go
@@ -20,9 +20,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 
-	"github.com/rudderlabs/keydb/internal/cache/memory"
-
 	"github.com/rudderlabs/keydb/client"
+	"github.com/rudderlabs/keydb/internal/cache/memory"
 	"github.com/rudderlabs/keydb/internal/cloudstorage"
 	pb "github.com/rudderlabs/keydb/proto"
 	"github.com/rudderlabs/rudder-go-kit/config"
