49f76c8187e79a6f755d51bd15ac75dfc2830bdd
Francesco Casula <fra.casula@gmail.com>
2025-08-08 14:24:39 +0200

    test: file assertions in operator

--- cmd/operator/server_test.go
+++ cmd/operator/server_test.go
@@ -57,7 +57,7 @@ func TestScaleUpAndDown(t *testing.T) {
 		return conf
 	}
 
-	_, cloudStorage := getCloudStorage(t, pool, newConf())
+	minioClient, cloudStorage := getCloudStorage(t, pool, newConf())
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -88,6 +88,12 @@ func TestScaleUpAndDown(t *testing.T) {
 
 	// Test CreateSnapshots
 	_ = op.Do("/createSnapshots", CreateSnapshotsRequest{NodeID: 0, FullSync: false}, true)
+
+	requireExpectedFiles(ctx, t, minioClient,
+		regexp.MustCompile("^hr_0_s_0_1.snapshot$"),
+		regexp.MustCompile("^hr_1_s_0_1.snapshot$"),
+		regexp.MustCompile("^hr_2_s_0_1.snapshot$"),
+	)
 
 	node1Conf := newConf()
 	node1, node1Address := getService(ctx, t, cloudStorage, node.Config{
@@ -133,6 +139,11 @@ func TestScaleUpAndDown(t *testing.T) {
 	})
 	require.JSONEq(t, `{"key1":true,"key2":true,"key3":true,"key4":false}`, body)
 
+	// Let's write something to generate more snapshots due to incremental updates
+	_ = op.Do("/put", PutRequest{
+		Keys: []string{"key5", "key6", "key7"}, TTL: testTTL,
+	}, true)
+
 	// Scale down by removing node1. Then node0 should pick up all keys.
 	// WARNING: when scaling up you can only add nodes to the right e.g. if the clusterSize is 2, and you add a node
 	// then it will be node2 and the clusterSize will be 3
@@ -140,6 +151,14 @@ func TestScaleUpAndDown(t *testing.T) {
 	// remove node0, you have to remove node1
 	_ = op.Do("/createSnapshots", CreateSnapshotsRequest{NodeID: 0, FullSync: false}, true)
 	_ = op.Do("/createSnapshots", CreateSnapshotsRequest{NodeID: 1, FullSync: false}, true)
+	requireExpectedFiles(ctx, t, minioClient,
+		regexp.MustCompile("^hr_0_s_0_1.snapshot$"),
+		regexp.MustCompile("^hr_0_s_1_2.snapshot$"),
+		regexp.MustCompile("^hr_1_s_0_1.snapshot$"),
+		regexp.MustCompile("^hr_1_s_1_2.snapshot$"),
+		regexp.MustCompile("^hr_2_s_0_1.snapshot$"),
+		regexp.MustCompile("^hr_2_s_1_2.snapshot$"),
+	)
 	_ = op.Do("/loadSnapshots", LoadSnapshotsRequest{
 		NodeID:     0,
 		HashRanges: hash.GetNodeHashRangesList(0, 1, totalHashRanges),
@@ -164,186 +183,6 @@ func TestScaleUpAndDown(t *testing.T) {
 	node1.Close()
 }
 
-func getService(
-	ctx context.Context, t testing.TB, cs filemanager.S3Manager, nodeConfig node.Config, conf *config.Config,
-) (*node.Service, string) {
-	t.Helper()
-
-	freePort, err := testhelper.GetFreePort()
-	require.NoError(t, err)
-	address := "localhost:" + strconv.Itoa(freePort)
-	nodeConfig.Addresses = append(nodeConfig.Addresses, address)
-
-	log := logger.NOP
-	if testing.Verbose() {
-		log = logger.NewLogger()
-	}
-	conf.Set("BadgerDB.Dedup.NopLogger", true)
-	service, err := node.NewService(ctx, nodeConfig, cs, conf, stats.NOP, log)
-	require.NoError(t, err)
-
-	// Create a gRPC server
-	server := grpc.NewServer()
-	pb.RegisterNodeServiceServer(server, service)
-
-	lis, err := net.Listen("tcp", address)
-	require.NoError(t, err)
-
-	// Start the server
-	go func() {
-		require.NoError(t, server.Serve(lis))
-	}()
-	t.Cleanup(func() {
-		server.GracefulStop()
-		_ = lis.Close()
-	})
-
-	return service, address
-}
-
-func createMinioResource(
-	t testing.TB,
-	pool *dockertest.Pool, accessKeyId, secretAccessKey, region, bucket string,
-) (string, *minio.Client) {
-	t.Helper()
-	// running minio container on docker
-	minioResource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "minio/minio",
-		Tag:        "latest",
-		Cmd:        []string{"server", "/data"},
-		Env: []string{
-			fmt.Sprintf("MINIO_ACCESS_KEY=%s", accessKeyId),
-			fmt.Sprintf("MINIO_SECRET_KEY=%s", secretAccessKey),
-			fmt.Sprintf("MINIO_SITE_REGION=%s", region),
-		},
-		ExposedPorts: []string{"9000/tcp"},
-	})
-	require.NoError(t, err)
-	t.Cleanup(func() { _ = pool.Purge(minioResource) })
-
-	minioEndpoint := fmt.Sprintf("localhost:%s", minioResource.GetPort("9000/tcp"))
-
-	// check if minio server is up & running.
-	err = pool.Retry(func() error {
-		url := fmt.Sprintf("http://%s/minio/health/live", minioEndpoint)
-		resp, err := http.Get(url)
-		if err != nil {
-			return err
-		}
-		defer func() { httputil.CloseResponse(resp) }()
-
-		if resp.StatusCode != http.StatusOK {
-			return fmt.Errorf("status code not OK")
-		}
-		return nil
-	})
-	require.NoError(t, err)
-
-	minioClient, err := minio.New(minioEndpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKeyId, secretAccessKey, ""),
-		Secure: false,
-	})
-	require.NoError(t, err)
-
-	// creating bucket inside minio where testing will happen.
-	err = minioClient.MakeBucket(context.Background(), bucket, minio.MakeBucketOptions{Region: region})
-	require.NoError(t, err)
-
-	return minioEndpoint, minioClient
-}
-
-func getCloudStorage(t testing.TB, pool *dockertest.Pool, conf *config.Config) (*minio.Client, filemanager.S3Manager) {
-	t.Helper()
-
-	minioEndpoint, minioClient := createMinioResource(t, pool, accessKeyId, secretAccessKey, region, bucket)
-	conf.Set("Storage.Bucket", bucket)
-	conf.Set("Storage.Endpoint", minioEndpoint)
-	conf.Set("Storage.AccessKeyId", accessKeyId)
-	conf.Set("Storage.AccessKey", secretAccessKey)
-	conf.Set("Storage.Region", region)
-	conf.Set("Storage.DisableSsl", true)
-	conf.Set("Storage.S3ForcePathStyle", true)
-	conf.Set("Storage.UseGlue", true)
-
-	cloudStorage, err := cloudstorage.GetCloudStorage(conf, logger.NOP)
-	require.NoError(t, err)
-
-	return minioClient, cloudStorage
-}
-
-func startOperatorHTTPServer(t testing.TB, totalHashRanges uint32, addresses ...string) *opClient { // nolint:unparam
-	t.Helper()
-
-	c, err := client.NewClient(client.Config{
-		Addresses:       addresses,
-		TotalHashRanges: totalHashRanges,
-	}, logger.NOP)
-	require.NoError(t, err)
-
-	op, err := operator.NewClient(operator.Config{
-		Addresses:       addresses,
-		TotalHashRanges: totalHashRanges,
-		RetryCount:      3,
-		RetryDelay:      time.Second,
-	}, logger.NOP)
-	require.NoError(t, err)
-
-	freePort, err := testhelper.GetFreePort()
-	require.NoError(t, err)
-
-	addr := fmt.Sprintf(":%d", freePort)
-
-	opServer := newHTTPServer(c, op, addr, logger.NOP)
-	go func() {
-		err := opServer.Start()
-		if !errors.Is(err, http.ErrServerClosed) {
-			t.Errorf("Operator server error: %v", err)
-		}
-	}()
-	t.Cleanup(func() {
-		_ = opServer.Stop(context.Background())
-	})
-
-	return &opClient{
-		t:      t,
-		client: http.DefaultClient,
-		url:    fmt.Sprintf("http://localhost:%d", freePort),
-	}
-}
-
-type opClient struct {
-	t      testing.TB
-	client *http.Client
-	url    string
-}
-
-func (c *opClient) Do(endpoint string, data any, success ...bool) string {
-	c.t.Helper()
-
-	buf, err := jsonrs.Marshal(data)
-	require.NoError(c.t, err)
-	req, err := http.NewRequest(http.MethodPost, c.url+endpoint, bytes.NewBuffer(buf))
-	require.NoError(c.t, err)
-
-	resp, err := c.client.Do(req)
-	require.NoError(c.t, err)
-
-	defer func() { httputil.CloseResponse(resp) }()
-
-	body, err := io.ReadAll(resp.Body)
-	require.NoError(c.t, err)
-
-	if resp.StatusCode != http.StatusOK {
-		c.t.Fatalf("unexpected status code: %d, body: %s", resp.StatusCode, string(body))
-	}
-
-	if len(success) > 0 && success[0] {
-		require.JSONEq(c.t, `{"success":true}`, string(body))
-	}
-
-	return string(body)
-}
-
 func TestAutoScale(t *testing.T) {
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err)
@@ -356,7 +195,7 @@ func TestAutoScale(t *testing.T) {
 		return conf
 	}
 
-	_, cloudStorage := getCloudStorage(t, pool, newConf())
+	minioClient, cloudStorage := getCloudStorage(t, pool, newConf())
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -414,6 +253,10 @@ func TestAutoScale(t *testing.T) {
 	require.EqualValues(t, 2, infoResponse.ClusterSize)
 	require.Len(t, infoResponse.NodesAddresses, 2)
 
+	requireExpectedFiles(ctx, t, minioClient,
+		regexp.MustCompile("^hr_1_s_0_1.snapshot$"),
+	)
+
 	// Verify data is still accessible after scale up
 	body = op.Do("/get", GetRequest{
 		Keys: []string{"key1", "key2", "key3", "key4"},
@@ -438,6 +281,13 @@ func TestAutoScale(t *testing.T) {
 		OldNodesAddresses: []string{node0Address, node1Address},
 		NewNodesAddresses: []string{node0Address},
 	}, true)
+
+	requireExpectedFiles(ctx, t, minioClient,
+		regexp.MustCompile("^hr_0_s_0_2.snapshot$"),
+		regexp.MustCompile("^hr_1_s_0_1.snapshot$"),
+		regexp.MustCompile("^hr_1_s_1_2.snapshot$"),
+		regexp.MustCompile("^hr_2_s_0_2.snapshot$"),
+	)
 
 	// Verify scale down worked - check node info
 	body = op.Do("/info", InfoRequest{NodeID: 0})
@@ -804,6 +654,186 @@ func TestHashRangeMovements(t *testing.T) {
 
 	cancel()
 	node0.Close()
+}
+
+func getService(
+	ctx context.Context, t testing.TB, cs filemanager.S3Manager, nodeConfig node.Config, conf *config.Config,
+) (*node.Service, string) {
+	t.Helper()
+
+	freePort, err := testhelper.GetFreePort()
+	require.NoError(t, err)
+	address := "localhost:" + strconv.Itoa(freePort)
+	nodeConfig.Addresses = append(nodeConfig.Addresses, address)
+
+	log := logger.NOP
+	if testing.Verbose() {
+		log = logger.NewLogger()
+	}
+	conf.Set("BadgerDB.Dedup.NopLogger", true)
+	service, err := node.NewService(ctx, nodeConfig, cs, conf, stats.NOP, log)
+	require.NoError(t, err)
+
+	// Create a gRPC server
+	server := grpc.NewServer()
+	pb.RegisterNodeServiceServer(server, service)
+
+	lis, err := net.Listen("tcp", address)
+	require.NoError(t, err)
+
+	// Start the server
+	go func() {
+		require.NoError(t, server.Serve(lis))
+	}()
+	t.Cleanup(func() {
+		server.GracefulStop()
+		_ = lis.Close()
+	})
+
+	return service, address
+}
+
+func createMinioResource(
+	t testing.TB,
+	pool *dockertest.Pool, accessKeyId, secretAccessKey, region, bucket string,
+) (string, *minio.Client) {
+	t.Helper()
+	// running minio container on docker
+	minioResource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "minio/minio",
+		Tag:        "latest",
+		Cmd:        []string{"server", "/data"},
+		Env: []string{
+			fmt.Sprintf("MINIO_ACCESS_KEY=%s", accessKeyId),
+			fmt.Sprintf("MINIO_SECRET_KEY=%s", secretAccessKey),
+			fmt.Sprintf("MINIO_SITE_REGION=%s", region),
+		},
+		ExposedPorts: []string{"9000/tcp"},
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() { _ = pool.Purge(minioResource) })
+
+	minioEndpoint := fmt.Sprintf("localhost:%s", minioResource.GetPort("9000/tcp"))
+
+	// check if minio server is up & running.
+	err = pool.Retry(func() error {
+		url := fmt.Sprintf("http://%s/minio/health/live", minioEndpoint)
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer func() { httputil.CloseResponse(resp) }()
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("status code not OK")
+		}
+		return nil
+	})
+	require.NoError(t, err)
+
+	minioClient, err := minio.New(minioEndpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyId, secretAccessKey, ""),
+		Secure: false,
+	})
+	require.NoError(t, err)
+
+	// creating bucket inside minio where testing will happen.
+	err = minioClient.MakeBucket(context.Background(), bucket, minio.MakeBucketOptions{Region: region})
+	require.NoError(t, err)
+
+	return minioEndpoint, minioClient
+}
+
+func getCloudStorage(t testing.TB, pool *dockertest.Pool, conf *config.Config) (*minio.Client, filemanager.S3Manager) {
+	t.Helper()
+
+	minioEndpoint, minioClient := createMinioResource(t, pool, accessKeyId, secretAccessKey, region, bucket)
+	conf.Set("Storage.Bucket", bucket)
+	conf.Set("Storage.Endpoint", minioEndpoint)
+	conf.Set("Storage.AccessKeyId", accessKeyId)
+	conf.Set("Storage.AccessKey", secretAccessKey)
+	conf.Set("Storage.Region", region)
+	conf.Set("Storage.DisableSsl", true)
+	conf.Set("Storage.S3ForcePathStyle", true)
+	conf.Set("Storage.UseGlue", true)
+
+	cloudStorage, err := cloudstorage.GetCloudStorage(conf, logger.NOP)
+	require.NoError(t, err)
+
+	return minioClient, cloudStorage
+}
+
+func startOperatorHTTPServer(t testing.TB, totalHashRanges uint32, addresses ...string) *opClient { // nolint:unparam
+	t.Helper()
+
+	c, err := client.NewClient(client.Config{
+		Addresses:       addresses,
+		TotalHashRanges: totalHashRanges,
+	}, logger.NOP)
+	require.NoError(t, err)
+
+	op, err := operator.NewClient(operator.Config{
+		Addresses:       addresses,
+		TotalHashRanges: totalHashRanges,
+		RetryCount:      3,
+		RetryDelay:      time.Second,
+	}, logger.NOP)
+	require.NoError(t, err)
+
+	freePort, err := testhelper.GetFreePort()
+	require.NoError(t, err)
+
+	addr := fmt.Sprintf(":%d", freePort)
+
+	opServer := newHTTPServer(c, op, addr, logger.NOP)
+	go func() {
+		err := opServer.Start()
+		if !errors.Is(err, http.ErrServerClosed) {
+			t.Errorf("Operator server error: %v", err)
+		}
+	}()
+	t.Cleanup(func() {
+		_ = opServer.Stop(context.Background())
+	})
+
+	return &opClient{
+		t:      t,
+		client: http.DefaultClient,
+		url:    fmt.Sprintf("http://localhost:%d", freePort),
+	}
+}
+
+type opClient struct {
+	t      testing.TB
+	client *http.Client
+	url    string
+}
+
+func (c *opClient) Do(endpoint string, data any, success ...bool) string {
+	c.t.Helper()
+
+	buf, err := jsonrs.Marshal(data)
+	require.NoError(c.t, err)
+	req, err := http.NewRequest(http.MethodPost, c.url+endpoint, bytes.NewBuffer(buf))
+	require.NoError(c.t, err)
+
+	resp, err := c.client.Do(req)
+	require.NoError(c.t, err)
+
+	defer func() { httputil.CloseResponse(resp) }()
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(c.t, err)
+
+	if resp.StatusCode != http.StatusOK {
+		c.t.Fatalf("unexpected status code: %d, body: %s", resp.StatusCode, string(body))
+	}
+
+	if len(success) > 0 && success[0] {
+		require.JSONEq(c.t, `{"success":true}`, string(body))
+	}
+
+	return string(body)
 }
 
 type File struct {
